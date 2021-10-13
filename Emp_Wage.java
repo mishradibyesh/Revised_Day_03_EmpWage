182a7317b38f0234cfdee8f32ea9119b6cb9b1e7
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-13 14:41:58 +0530

    adding part time employee

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -1,19 +1,33 @@
 package com.bl.empwage;
 
 public class Emp_Wage {
+	static final int FULLTIME = 1;
+	static final int  PART_TIME = 2;
 	public static void main(String[] args) {
 		System.out.println("Welcome to EMPLOYEE WAGE COMPUTAION PROGRAM");
-		int Is_Present=(int)Math.floor(Math.random()*10)%2;                    //generating  random values 0 and 1
-		int salary=0;
-		if(Is_Present==1)                                                      // checking presence with the help of if condition
+		int Random = (int)Math.floor(Math.random()*10)%3;                 //GENERATING random values
+		int salary = 0;
+		int FullDayHour = 0;
+		int WagePerHour = 0;
+		if(Random == FULLTIME)                                           // checking presence with the help of if condition
 		{	
-			int FullDayHour=8;
-			int WagePerHour=20;
-			salary+=WagePerHour*FullDayHour;
-			System.out.println("Employee is present and his today's income is Rs. "+salary);
-
+			FullDayHour = 8;
+			WagePerHour = 20;
+			System.out.print("Employee is full time and ");
 		}
-		else
-			System.out.println("Employee is absent and his today's income is Rs. "+salary);
+		                                                             // checking if employee is part time
+		else if(Random == PART_TIME) {
+			FullDayHour = 4;
+			WagePerHour = 20;
+			System.out.print("Employee is part time and ");
+		}
+
+		else if (Random == 0) { 
+			FullDayHour = 0;
+			WagePerHour = 0;
+			System.out.print("Employee is absent and ");
+		}
+		salary += WagePerHour*FullDayHour;                             // calculating salary
+		System.out.println("his income is Rs."+salary);
 	}
 }
