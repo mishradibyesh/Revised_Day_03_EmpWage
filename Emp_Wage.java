5d96d2c8ad8b5fe9dd249bc56d373d773e521399
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-13 14:34:50 +0530

    checking presence of employee

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -1,5 +1,11 @@
 package com.bl.empwage;
 
 public class Emp_Wage {
-
+	public static void main(String[] args) {	
+		System.out.println("Welcome to EMPLOYEE WAGE COMPUTAION PROGRAM");
+		int Is_Present = (int)Math.floor(Math.random() * 10) % 2;                //generating random number i.e 0 or 1
+		if(Is_Present == 1)                                                 // checking presence with the help of if condition
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent"); }
 }
