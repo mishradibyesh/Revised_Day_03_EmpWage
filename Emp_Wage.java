1e98b91ea916b873e2007eb584cab060f42ca7f5
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-19 00:37:40 +0530

    storing wages of diffrent comapnies in an array

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 public class Emp_Wage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to EMPLOYEE WAGE COMPUTAION PROGRAM");
-		ArrayList<CalculateWage> data = new ArrayList<CalculateWage>();
+		CalculateWage[] data = new CalculateWage[2];   // to store wages of different companies 
 		CalculateWage calc = new CalculateWage(); // creating reference variable for class calculatewage
 		calc.EmpWageBuilder("FlipKart",50,25,100);
-		data.add(calc);
-		CalculateWage calc2 = new CalculateWage(); // creating reference variable for class calculatewage
+		data[0] = calc ;                            // storing the the calculatewage objects in array
+		CalculateWage calc2 = new CalculateWage(); // creating 2nd reference variable for class calculatewage
 		calc2.EmpWageBuilder("dmart",33,24,100);
-		data.add(calc2);
-		for(int i =0 ;i< data.size();i++) {
-			System.out.println(data.get(i));
-		}
+		data[1] = calc2 ;                          // storing the the calculatewage objects in array
 
+		for(int i = 0; i<2 ; i++ ) {               // printing the wages of different comapanies
+			System.out.println(data[i]);
+		}
 	}
 }
